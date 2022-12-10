52845e908277643b8fad7b9cbcc139165c34d5c2
Arjun Gandhi <arjun.gandhi9777@gmail.com>
2022-12-10 14:46:28 -0500

    Sorted array linear search computation steps

---
+++ method2.py
@@ -0,0 +1,3 @@
+array = ["Hello! ", "How are you ", "today?"]
+print(array[0] + array[1] + array [2])
+print (len(array))
---
+++ sorted_list_linear_search.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+
+def linear_search(list_of_element, search_number):
+    """
+       NOTE: This is just for educational purpose to show
+       how different algo and data structure affect computation steps
+       Iterate through list_of_element,
+       breakout if a list value is greater than search_number instead of 
+       iterating through all elements of a list
+    """
+    for key, value in enumerate(list_of_element):
+        print(f"search for element at index {key} and value {value}")
+        if value == search_number:
+            print(f"found match at index {key} and {value}")
+            break
+        elif (value > search_number):
+            break
+    print ("out of for loop")
+
+linear_search([3,17,75,80,202], 22)
+
+
