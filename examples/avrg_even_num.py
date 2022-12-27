c2f8f4b23c79762d78845d66692b896b9dd6ef58
Arjun Gandhi <arjun.gandhi9777@gmail.com>
2022-12-27 10:23:52 -0500

    example

---
+++ examples/avrg_even_num.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+def average_even_numbers(listA : list):
+    sum = 0
+    count_of_event_number = 0
+    for key, value in enumerate(listA):
+        print(f"Iterating for loop key : {key} and value : {value} ")
+        if abs(value) % 2 == 0:
+            sum += abs(value)
+            count_of_event_number += 1
+    return sum / count_of_event_number
+
+
+a = [1,2,3,-4,5,-6,7,-8,9,10]
+x = average_even_numbers(a)
+print(f"Average mean of event numbers : {x}")
