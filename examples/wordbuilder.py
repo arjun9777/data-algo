4ec721ec8a2560a4c60ddc53d6750eedee827393
Arjun Gandhi <arjun.gandhi9777@gmail.com>
2022-12-27 11:36:26 -0500

    example

---
+++ examples/wordbuilder.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+
+def buildwords(listA: list) -> list:
+    collect = []
+    for key, value in enumerate(listA):
+        print(f"Outer for loop key : {key} and value : {value}")
+        for key2, value2 in enumerate(listA):
+            print(f"************* Inner for loop key : {key2} and value : {value2}")
+            if key != key2:
+                collect.append(value+value2)
+    return collect
+
+a = ["a", "b", "c", "d"]
+x = buildwords(a)
+print(f"combination of two characters strings {x}")
