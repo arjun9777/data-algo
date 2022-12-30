b0c5999ca39b3d45c5fca3b6a279864711e47047
Arjun Gandhi <arjun.gandhi9777@gmail.com>
2022-12-30 16:57:48 -0500

    example

--- examples/palindrome_checker.py
+++ examples/palindrome_checker.py
@@ -1,2 +1,20 @@
 #! /usr/bin/env python3
 
+def ispalindrome(listA: list) -> bool:
+    leftIndex = 0
+    rightIndex = len(listA) - 1
+
+    while (leftIndex < len(listA)/2):
+        if listA[leftIndex] != listA[rightIndex]:
+            return False
+    
+        leftIndex +=1
+        rightIndex -=1
+    return True
+
+a = ['r','a','c','e','c','a','r']
+b = ['a', 'p', 'p','l','e']
+x = ispalindrome(b)
+
+print(f"Is it palindrome : {x}")
+
