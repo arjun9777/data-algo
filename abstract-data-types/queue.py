7c710d2032c5d33e28a1559b6a90ffb6d1ad8651
Arjun Gandhi <arjun.gandhi9777@gmail.com>
2023-01-12 21:48:22 -0500

    Abstract data type example

---
+++ abstract-data-types/queue.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+from collections import deque
+
+
+def printerqueue(filearray: list):
+    myqueue = deque()
+
+    for i in filearray:
+        myqueue.append(i)
+    print(f"Queue values : {myqueue}") 
+    
+    try:
+        while (x:=myqueue.popleft()):
+            print(x)
+    except:
+        pass
+    return "Please check printer to collect your pages"
+
+
+a = ["hello", "left", "to", "right"]
+b = ["First Document" , "second Document", "Third Document"]
+
+y = printerqueue(b)
+
+print(f"Printing : {y}")
