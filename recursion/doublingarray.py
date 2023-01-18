64d00735a24486f2c41670d2b718002c1172a373
Arjun Gandhi <arjun.gandhi9777@gmail.com>
2023-01-17 22:26:36 -0500

    example

--- recursion/doublingarray.py
+++ recursion/doublingarray.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python3
 
-def double_array(array, index):
+def double_array(array : list, index = 0):
     if index >= len(array):
         return
     array[index] *= 2     
     double_array(array, index + 1)
 
 a = [1,2,3,4]
-double_array(a, 0)
+double_array(a)
 print(f"Doubling values of the array : {a}")
